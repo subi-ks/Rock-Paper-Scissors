77eb6ef04cbb439733aecf102f31431d80a74a7d
Subiksha <subi.ks97@gmail.com>
2020-08-04 23:03:29 +0530

    Rough draft

---
+++ gameSciroper.py
@@ -0,0 +1,27 @@
+"""
+		ROCK-PAPER_SCISSORS
+
+ Author: K. S. Subiksha
+ Version: 0.1
+ Description: Read README.md file
+"""
+
+#load libraries if any
+
+#Number of players - 2
+playerName = ""
+#Main elements in this game - Rock, Paper, Scissors
+gameElement = ['Rock', 'Paper', 'Scissors']
+
+def gameIntro():
+	print("Welcome to Rock- Paper-Scissors!\n")
+	playerName = input(print("I am Sciroper. What is your good name?"))
+
+def numberOfRounds():
+	print
+
+#Game introduction
+gameIntro()
+
+#Number of rounds
+numberOfRounds()
